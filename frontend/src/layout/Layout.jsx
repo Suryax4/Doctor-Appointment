1024ec7681dffbae23cdfb80cd0d4c5fca40d06e
Surya Prakash <suryaprakashx4@gmail.com>
2023-10-06 20:45:28 +0530

    Header Section Done

--- frontend/src/layout/Layout.jsx
+++ frontend/src/layout/Layout.jsx
@@ -1,6 +1,6 @@
-import React from "react";
 import Header from "../components/Header/Header";
 import Routers from "../routes/Routers";
+import Footer from "../components/Footer/Footer";
 
 const Layout = () => {
   return (
--- frontend/src/routes/Routers.jsx
+++ frontend/src/routes/Routers.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import Doctors from "../pages/Doctors/Doctors";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import Contact from "../pages/Contact";
+import Services from "../pages/Services";
+import DoctorDetails from "../pages/Doctors/DoctorDetails";
 
 const Routers = () => {
-  return <div>Routers</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/home" element={<Home />}></Route>
+      <Route path="/doctors" element={<Doctors />}></Route>
+      <Route path="/doctors/:id" element={<DoctorDetails />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="/register" element={<Signup />}></Route>
+      <Route path="/contact" element={<Contact />}></Route>
+      <Route path="/services" element={<Services />}></Route>
+    </Routes>
+  );
 };
 
 export default Routers;
