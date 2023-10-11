297f3f59e87c9affaa61dcdaa838ae5112313d81
Surya Prakash <suryaprakashx4@gmail.com>
2023-10-11 14:35:11 +0530

    About and Service Section Done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,6 +6,8 @@ import icon02 from "../assets/images/icon02.png";
 import icon03 from "../assets/images/icon03.png";
 import { BsArrowRight } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import About from "../components/About/About";
+import ServiceList from "../components/Services/ServiceList";
 
 const Home = () => {
   return (
@@ -151,6 +153,28 @@ const Home = () => {
           </div>
         </div>
       </section>
+
+      {/* --------------------About Section Start-------------  */}
+
+      <About />
+
+      {/* ---------------------About Section End--------------- */}
+
+      {/* -------------------Services Section----------------  */}
+      <section>
+        <div className="container">
+          <div className="xl:w-[470]px mx-auto">
+            <h2 className="heading text-center"> Our Medical Services</h2>
+            <p className="text__para text-center">
+              World-class care for everyone.Our health System offers unmatched,
+              expert health care
+            </p>
+          </div>
+          <ServiceList />
+        </div>
+      </section>
+
+      {/* -----------------Service Section End---------------  */}
     </>
   );
 };
